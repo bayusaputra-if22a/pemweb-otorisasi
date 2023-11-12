63cf1f9a212d5899fc667092bf585c5c3b9d7b9b
bayusaputra-if22a <150505916+bayusaputra-if22a@users.noreply.github.com>
2023-11-12 23:01:55 +0700

    Update pemweb-db.sql

--- pemweb-db.sql
+++ pemweb-db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 11 Nov 2023 pada 14.05
+-- Waktu pembuatan: 12 Nov 2023 pada 17.00
 -- Versi server: 10.4.28-MariaDB
 -- Versi PHP: 8.2.4
 
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `pemweb-db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `image`, `created_at`, `updated_at`) VALUES
+(41, 'Bayu Saputra ', 321312, '/upload/1699804643-0322d80b332099b83340bb41423d938f-tugas6.1.png', '2023-11-02 06:36:06', NULL),
+(42, '370 Diamond', 777, '/upload/1699804691-fd05ef827d119d3ca5bdf6036ebfc6ac-obat3.jpeg', '2023-11-02 06:42:30', NULL);
 
 -- --------------------------------------------------------
 
@@ -44,7 +67,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `role`, `password`, `photo`, `created_at`, `updated_at`) VALUES
 (14, 'Bayu Saputra ', 'bayu@bayu', 'user', '$2y$10$3OW.Ubmsn9PlhjjVsHx3/.RXBfUc6cabVav5AoLGoA0m2QeBsE1NW', NULL, '2023-11-10 07:32:01', NULL),
-(15, 'User', 'user@user', 'user', '$2y$10$4tkZU3DmB0Mw4MBdZGQU4u5pVmXTp3jcliuk3MZDmO4GmdPDPHwKW', NULL, '2023-11-10 08:13:16', NULL);
+(15, 'User', 'user@user', 'user', '$2y$10$4tkZU3DmB0Mw4MBdZGQU4u5pVmXTp3jcliuk3MZDmO4GmdPDPHwKW', NULL, '2023-11-10 08:13:16', NULL),
+(16, 'Bayu Saputra ', 'bayu@uyab', 'user', '$2y$10$11PnKMEDYvozaWmvi/RsEOvonZpSxbmnKjifGgnZ/HvblPmCm0XFm', NULL, '2023-11-11 07:19:43', NULL);
 
 --
 -- Indexes for dumped tables
@@ -65,7 +89,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
