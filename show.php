45b88904e7c71d8ba1dadafe838f7b05487f893d
bayusaputra-if22a <150505916+bayusaputra-if22a@users.noreply.github.com>
2023-11-13 11:44:24 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,14 +1,12 @@
 <?php 
 session_start();
 if ($_SESSION['role'] != 'admin') {
-    // session_destroy();
     echo "
     <script>
     alert('Halaman ini hanya bisa di akses oleh admin');
     window.location = 'profile.php';
     </script>
     ";
-    // header("location:profile.php");
 }
 ?>
 <!DOCTYPE html>
@@ -17,9 +15,32 @@ if ($_SESSION['role'] != 'admin') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./plugin/css/bootstrap.min.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <title>Products</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Bayu Web</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="./index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">History</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Products</a>
+              </li>
+            </ul>
+              <a href="backend/logout.php" class="btn btn-danger">Logout</a>
+          </div>
+        </div>
+</nav>
     <h1>Data produk</h1>
     <div style="border: none !important" class="card m-1">
       <div class="card-body">
@@ -31,8 +52,8 @@ if ($_SESSION['role'] != 'admin') {
             <tr>
                 <th scope="col">#</th>
                 <th scope="col">Nama produk</th>
-                <th scope="col">harga</th>
-                <th scope="col">gambar produk</th>
+                <th scope="col">Harga</th>
+                <th scope="col">Gambar Produk</th>
                 <th scope="col">Opsi</th>
             </tr>
         </thead>
@@ -49,12 +70,12 @@ if ($_SESSION['role'] != 'admin') {
                 <tr>
                     <td><?=$no++;?></td>
                     <td><?=$row['name'];?></td>
-                    <td><?=$row['price'];?></td>
+                    <td>Rp <?= number_format($row['price'], 0, ',', '.'); ?></td>
                     <!-- <td><img src="<?=$row['image'];?>" width="100"></td> -->
-                    <td><a href="<?php echo $base_url ?><?=$row['image'];?>" target="_blank">Lihat</a></td>
+                    <td><a class="btn btn-info" href="<?php echo $base_url ?><?=$row['image'];?>" target="_blank"><i class="fa fa-eye" aria-hidden="true"></i> Lihat</a></td>
                     <td>
-                        <a href="backend/edit.php?id=<?=$row['id'];?>">Edit</a>
-                        <a href="backend/delete.php?id=<?=$row['id'];?>">Hapus</a>
+                        <a class="btn btn-warning" href="backend/edit.php?id=<?=$row['id'];?>"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</a>
+                        <button onclick="hapus(<?php echo $row['id']; ?>)" class="btn btn-danger"><i class="fa fa-trash" aria-hidden="true"></i> Hapus</button>
 
                     </td>
                 </tr>
@@ -63,5 +84,17 @@ if ($_SESSION['role'] != 'admin') {
     </table>
       </div>
     </div>
+    <script src="../plugin/js/bootstrap.min.js"></script>
+    <script>
+    function hapus(id_user){
+        var konfirmasi = confirm("Anda yakin ingin menghapus data ini?");
+        if(konfirmasi == true){
+            window.location.href = "backend/delete.php?id=" + id_user;
+        }
+        else {
+            return false;
+        }
+    }
+</script>
 </body>
 </html>
