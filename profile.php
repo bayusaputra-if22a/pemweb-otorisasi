69a861caaa1f1db20824c07afa43c5c15aae5c59
bayusaputra-if22a <150505916+bayusaputra-if22a@users.noreply.github.com>
2023-11-13 11:44:04 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -10,10 +10,32 @@ if ($_SESSION['role'] != 'user') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./plugin/css/bootstrap.min.css">
+    <title>User</title>
 </head>
 <body>
-    <h1>Selamat datang <?php echo $_SESSION['name']?></h1>
-    <a href="backend/logout.php">Logout</a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Bayu Web</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="profile.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">History</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="show.php">Products</a>
+              </li>
+            </ul>
+              <a href="backend/logout.php" class="btn btn-danger">Logout</a>
+          </div>
+        </div>
+</nav>
+    <h1>Selamat datang user: <?php echo $_SESSION['name']?></h1>
 </body>
 </html>
