aaa5849efe5ff376c510a851a68dfb070a8e0597
bayusaputra-if22a <150505916+bayusaputra-if22a@users.noreply.github.com>
2023-11-13 11:42:15 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,8 +1,12 @@
 <?php 
 session_start();
 if ($_SESSION['role'] != 'admin') {
-    // session_destroy();
-    header("location:profile.php");
+    echo "
+    <script>
+    alert('Halaman ini hanya bisa di akses oleh admin');
+    window.location = 'profile.php';
+    </script>
+    ";
 }
 ?>
 <!DOCTYPE html>
@@ -10,10 +14,32 @@ if ($_SESSION['role'] != 'admin') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./plugin/css/bootstrap.min.css">
+    <title>Admin</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Bayu Web</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">History</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="show.php">Products</a>
+              </li>
+            </ul>
+              <a href="backend/logout.php" class="btn btn-danger">Logout</a>
+          </div>
+        </div>
+</nav>
     <h1>Selamat datang Administrator: <?php echo $_SESSION['name']?></h1>
-    <a href="backend/logout.php">Logout</a>
 </body>
 </html>
